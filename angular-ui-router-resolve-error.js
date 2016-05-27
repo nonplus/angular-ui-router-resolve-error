1b566edbcfe1cc1de2237232eb1df883c0c5bcb0
Stepan Riha <github@nonplus.net>
2016-05-27 17:36:47 -0500

    Bumped version number

--- angular-ui-router-resolve-error.js
+++ angular-ui-router-resolve-error.js
@@ -3,7 +3,7 @@
  *
  * @link https://github.com/nonplus/angular-ui-router-resolve-error#readme
  *
- * @license angular-ui-router-resolve-error v0.0.1
+ * @license angular-ui-router-resolve-error v0.0.2
  * (c) Copyright Stepan Riha <github@nonplus.net>
  * License MIT
  */
@@ -15,7 +15,7 @@ angular.module("ui.router.resolve-error", [])
 	.run(["$rootScope", function($rootScope) {
 
 		$rootScope.$on( '$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-			if (error.message.match(/\[\$injector:unpr\]/)) {
+			if (error && typeof error.message === "string" && error.message.match(/\[\$injector:unpr\]/)) {
 				// Log missing 'resolve' dependency
 				// See: https://github.com/angular-ui/ui-router/issues/469
 				console && console.error("Invalid resolve in state '" + toState.name + "' - " + error.message);
